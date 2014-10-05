fa105b60e58c0bad20d12ecaebc952abd28ecb6a
dtsbourg <dylan.bourgeois@epfl.ch>
2014-10-05 21:00:26 +0200

    Initial Commit

---
+++ SwiftInterpreter/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SwiftInterpreter
+//
+//  Created by Dylan Bourgeois on 05/10/14.
+//  Copyright (c) 2014 Dylan Bourgeois. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
